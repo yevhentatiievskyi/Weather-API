88440a4865bc5450e7c1ce0cc669903dfff63548
k.martina511@gmail.com <Stickers23>
2019-10-29 20:01:32 +0200

    put configs into separated file

--- faker/index.js
+++ faker/index.js
@@ -1,12 +1,9 @@
-const express = require("express");
+const express = require('express');
 const axios = require('axios');
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const configs = require('./configs');
 const app = express();
-
-const WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/weather';
-const WEATHER_API_KEY = '839cf81f09a2b478e1d82492e77953de';
-const PORT = '3006';
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,9 +13,9 @@ async function getWeather (request, response) {
     try {
       const params = {
         q: request.params.city,
-        APPID: WEATHER_API_KEY
+        APPID: configs.WEATHER_API_KEY
       };
-      const weather = await axios.get(WEATHER_API_URL, { params });
+      const weather = await axios.get(configs.WEATHER_API_URL, { params });
       response.status(200).json(weather.data);
     }
     catch (e) {
@@ -27,5 +24,5 @@ async function getWeather (request, response) {
 }
 
 app.use('/v1/weather/:city', getWeather);
-console.log(`Server started on port ${PORT}`);
-app.listen(PORT);
+console.log(`Server started on port ${configs.PORT}`);
+app.listen(configs.PORT);
